5be03d720d9caaa73c26f2747cd6da9d57c5ff8a
alejandro-silver <fleacr3@gmail.com>
2019-11-11 21:36:59 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require('express');
 const body_parser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
 const client_sessions = require('client-sessions');
 const express_fileupload = require('express-fileupload');
-const models = require('./models');
-const { fn, col, Op } = require('sequelize');
-const cloudinary_manager = require('./cloudinary_manager');
 const cors = require('cors');
-
 const GetRouter = require('./routers/get-router');
 const PostRouter = require('./routers/post-router');
 const PutRouter = require('./routers/put-router');
 const DeleteRouter = require('./routers/delete-router');
-
-
+/** Setup */
 const app = express();
 const port = process.env.PORT || 4000;
-
 require('dotenv').config()
-app.use(cors());
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(client_sessions({
@@ -32,21 +24,41 @@ app.use(client_sessions({
 	  ephemeral: false
   }
 }));
-
 app.use(express_fileupload({ safeFileNames: true, preserveExtension: true }));
-
-
+// setup cors policy
+const whitelist_domains = [
+  // dev origin(s)
+  'http://localhost:3000',
+  // prod origin(s)
+  'http://haboclient.herokuapp.com',
+];
+const corsOptions = {
+  // https://expressjs.com/en/resources/middleware/cors.html
+  origin: function (origin, callback) {
+    const originIsAllowed = whitelist_domains.includes(origin);
+    console.log({
+      origin,
+      callback,
+      originIsAllowed,
+    });
+    if (originIsAllowed) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.options('*', cors(corsOptions));
 // Mount Routers On Main Express App
-app.use('/', GetRouter);
-app.use('/', PostRouter);
-app.use('/', PutRouter);
-app.use('/', DeleteRouter);
-
-
+app.use('/', cors(corsOptions), GetRouter);
+app.use('/', cors(corsOptions), PostRouter);
+app.use('/', cors(corsOptions), PutRouter);
+app.use('/', cors(corsOptions), DeleteRouter);
 /**
  * 
  */
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
